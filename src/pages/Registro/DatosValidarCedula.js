763f3eb2faa749281f7c8770844dc38a6801b033
Gabo-Puck <melkorgodo@gmail.com>
2023-05-25 00:23:21 -0600

    Añadido: Verificacion de cedula ingresada existente en sistema de cedulasprofesionales.com

--- src/pages/Registro/DatosValidarCedula.js
+++ src/pages/Registro/DatosValidarCedula.js
@@ -13,14 +13,16 @@ import {
 } from "@mantine/core";
 import { Dropzone, IMAGE_MIME_TYPE } from "@mantine/dropzone";
 import { useEffect, useState } from "react";
-import { FaUserAlt } from "react-icons/fa";
+import { FaCheck, FaUserAlt } from "react-icons/fa";
 import { MdOutlineVerified } from "react-icons/md";
 import { DisabledButton, EnabledButton } from "../../Components/DynamicButtons";
 import { useForm } from "@mantine/form";
-import { useOutletContext } from "react-router-dom";
+import { useNavigate, useOutletContext } from "react-router-dom";
 import { executeValidation } from "../../utils/isFormInvalid";
 import { isRequiredValidation } from "../../utils/inputValidation";
 import axios from "axios";
+import { notifications } from "@mantine/notifications";
+import { ImCross } from "react-icons/im";
 
 export function DatosValidarCedula({ anterior, siguiente }) {
   const [isLoading, setIsLoading] = useState(false);
@@ -28,84 +30,61 @@ export function DatosValidarCedula({ anterior, siguiente }) {
   const [urlImagen, setUrlImagen] = useState();
   const [file, setFile] = useState();
   const { datos, setDatos } = useOutletContext();
-  const { especialidad, numero_cedula } = datos;
+  const { numero_cedula } = datos;
+  const navigate = useNavigate();
   const [isDisabledCedulaFoto, setIsDisabledCedulaFoto] = useState(true);
   const [isValidandoNumCedula, setIsValidandoNumCedula] = useState(false);
   const form = useForm({
     validateInputOnBlur: true,
     validateInputOnChange: true,
     initialValues: {
-      especialidad: especialidad,
       numero_cedula: numero_cedula,
     },
     validate: {
-      especialidad: (value) => executeValidation(value, [isRequiredValidation]),
       numero_cedula: (value) =>
         executeValidation(value, [isRequiredValidation]),
     },
   });
-  useEffect(() => {
-    fetch(
-      "https://www.cedulaprofesional.sep.gob.mx/cedula/presidencia/indexAvanzada.action",
-      {
-        headers: {
-          accept:
-            "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
-          "accept-language": "es-ES,es;q=0.9",
-          "cache-control": "max-age=0",
-          "sec-ch-ua":
-            '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-            "Set-Cookie":"",
-          "sec-ch-ua-mobile": "?0",
-          "sec-ch-ua-platform": '"Windows"',
-          "sec-fetch-dest": "document",
-          "sec-fetch-mode": "navigate",
-          "sec-fetch-site": "none",
-          "sec-fetch-user": "?1",
-          "upgrade-insecure-requests": "1",
-        },
-        referrerPolicy: "strict-origin-when-cross-origin",
-        body: null,
-        method: "GET",
-        mode: "no-cors",
-        credentials: "include",
-      }
-    ).then(() => {
-      fetch(
-        "https://www.cedulaprofesional.sep.gob.mx/cedula/buscaCedulaJson.action",
-        {
-          headers: {
-            accept: "*/*",
-            "accept-language": "es-ES,es;q=0.9",
-            "content-type": "application/x-www-form-urlencoded; charset=UTF-8",
-            "sec-ch-ua":
-              '"Google Chrome";v="113", "Chromium";v="113", "Not-A.Brand";v="24"',
-            "sec-ch-ua-mobile": "?0",
-            "sec-ch-ua-platform": '"Windows"',
-            "sec-fetch-dest": "empty",
-            "sec-fetch-mode": "cors",
-            "sec-fetch-site": "same-origin",
-            "x-requested-with": "XMLHttpRequest",
-          },
-          referrer:
-            "https://www.cedulaprofesional.sep.gob.mx/cedula/presidencia/indexAvanzada.action",
-          referrerPolicy: "strict-origin-when-cross-origin",
-          body: "json=%7B%22maxResult%22%3A%221000%22%2C%22nombre%22%3A%22%22%2C%22paterno%22%3A%22%22%2C%22materno%22%3A%22%22%2C%22idCedula%22%3A%226091402%22%7D",
-          method: "POST",
-          mode: "no-cors",
-          credentials: "include",
-        }
-      ).then((res)=>res.json()).then(res=>console.log("RESPONSE CEDULA: ",res)).catch(e=>console.log("Fallo request json",e));
-    }).catch(err=>console.log("Fallo request inicial: ",err));
-  }, []);
-  const irAtras = () => {};
+  useEffect(() => {}, []);
+  const irAtras = () => {
+    navigate(anterior);
+  };
   const irSiguiente = () => {};
   const buscarCedula = async () => {
-    await axios.post(
-      "https://www.cedulaprofesional.sep.gob.mx/cedula/buscaCedulaJson.action",
-      {}
-    );
+    try {
+      let response = await axios.post(
+        `${process.env.REACT_APP_BACKEND_API}/utilidades/validarCedula`,
+        {
+          cedula: form.values.numero_cedula,
+        }
+      );
+      notifications.show({
+        message: "Se ha encontrado tu cedula!",
+        autoClose: 3500,
+        icon: <FaCheck />,
+        color: "green-nature",
+      });
+      setIsDisabledCedulaFoto(false);
+    } catch (err) {
+      if (!err.response) {
+        notifications.show({
+          message: "Ha habido un error, intenta más tarde",
+          autoClose: 3500,
+          icon: <ImCross />,
+          color: "red",
+        });
+        console.log("ERROR: ", err);
+      } else {
+        setIsDisabledCedulaFoto(true);
+        let mensaje = err.response.data;
+        form.setErrors({ numero_cedula: mensaje });
+      }
+    }
+    setIsValidandoNumCedula(false);
   };
+  useEffect(() => {
+    setDatos({ ...datos, ...form.values });
+  }, [form.values]);
   return (
     <Stack pos="relative">
       <Box mx="auto" pos={"relative"}>
@@ -134,13 +113,18 @@ export function DatosValidarCedula({ anterior, siguiente }) {
             loading={isValidandoNumCedula}
             onClick={() => {
               setIsValidandoNumCedula(true);
+              buscarCedula();
             }}
             color="green-nature"
           >
             Validar
           </Button>
-          {isDisabledCedulaFoto && (
+          {!isDisabledCedulaFoto && (
             <>
+              <Text color="dimmed">
+                Sube una foto de tu cedula para poder validarla con los datos
+                que has ingresado
+              </Text>
               <Dropzone
                 accept={IMAGE_MIME_TYPE}
                 onDrop={([file]) => {
@@ -161,13 +145,6 @@ export function DatosValidarCedula({ anterior, siguiente }) {
                   <Text align="center">Sube una foto de tu cedula</Text>
                 )}
               </Dropzone>
-              <TextInput
-                label="Especialidad"
-                placeholder=""
-                mt="lg"
-                withAsterisk
-                {...form.getInputProps("especialidad")}
-              />
             </>
           )}
         </Stack>
