d4e28211c82dd2687dcc150a9987f32116b0c222
Gabo-Puck <melkorgodo@gmail.com>
2023-06-13 11:47:49 -0600

    Cambiado: API KEY de firebase

--- src/firebase.js
+++ src/firebase.js
@@ -13,14 +13,15 @@ import {
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCbFXk2UKbPqS_fzsfkCjySpDuwYONEKAA",
+  apiKey: "AIzaSyBuVuRmAVjQVttgozamfi5ceKhYKFlTQFw",
   authDomain: "kbocchi-1254b.firebaseapp.com",
   projectId: "kbocchi-1254b",
   storageBucket: "kbocchi-1254b.appspot.com",
   messagingSenderId: "280897534781",
   appId: "1:280897534781:web:880b1ec78fc9ea2b3e6354",
-  measurementId: "G-FPWW2DZD5S",
+  measurementId: "G-FPWW2DZD5S"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
