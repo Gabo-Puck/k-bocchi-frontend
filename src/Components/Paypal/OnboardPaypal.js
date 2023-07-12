3d7f3b39bb4c2075e0826eebaaeae5327d4af083
Gabo-Puck <melkorgodo@gmail.com>
2023-07-12 02:26:48 -0600

    Añadido: Boton para vincular cuenta de Paypal

--- src/Components/Paypal/OnboardPaypal.js
+++ src/Components/Paypal/OnboardPaypal.js
@@ -2,26 +2,57 @@ import axios from "axios";
 import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { selectUsuario } from "../../utils/usuarioHooks";
+import {
+  Badge,
+  Button,
+  Group,
+  Skeleton,
+  Stack,
+  Text,
+  Title,
+} from "@mantine/core";
+import { ReactComponent as PaypalSvg } from "../../resources/svg/paypalSvg.svg";
+
+const ACTIVO = "Activo";
+const INACTIVO = "Inactivo";
 export default function OnboardPaypal() {
   const { terapeuta } = useSelector(selectUsuario);
   const [links, setLinks] = useState();
-  const [showButton, setShowButton] = useState(false);
-  const handleScriptLoad = () => {
-    // Aquí puedes realizar cualquier acción necesaria después de que el script se haya cargado
-    console.log("Script de PayPal cargado correctamente");
-  };
+  const [showStatus, setShowStatus] = useState(false);
+  const [showVincular, setShowVincular] = useState(false);
+  const [scriptLoaded, setScriptLoaded] = useState(false);
+  const [status, setStatus] = useState();
+  const [merchantId, setMerchantId] = useState();
   const getOnboardLinks = async () => {
     if (!terapeuta) return;
     let { id: id_terapeuta } = terapeuta;
+    let errorResponse;
     try {
-      let { data } = await axios.get(
+      let { data: onboardStatus } = await axios.get(
+        `/pagos/onboard-status/${id_terapeuta}`
+      );
+      setStatus(ACTIVO);
+      setMerchantId(onboardStatus.merchant_id);
+      console.log(onboardStatus);
+      return;
+    } catch (err) {
+      errorResponse = err;
+    }
+    try {
+      if (!errorResponse) return;
+      let { response } = errorResponse;
+      let { data: onboardLinks } = await axios.get(
         `/pagos/create-onboard-seller/${id_terapeuta}`
       );
-      console.log(data.links);
-      setLinks(data.links);
+      setLinks(onboardLinks.links);
+      console.log(onboardLinks.links);
+      setStatus(INACTIVO);
+      if (response.status === 400) {
+        setMerchantId(response.data.merchant_id);
+      }
+      setShowVincular(true);
     } catch (err) {
       console.log(err);
-      return null;
     }
   };
   useEffect(() => {
@@ -35,10 +66,11 @@ export default function OnboardPaypal() {
       const handleScriptLoad = () => {
         // Aquí puedes realizar cualquier acción necesaria después de que el script se haya cargado
         console.log("Script de PayPal cargado correctamente");
-        setShowButton(true);
+        setScriptLoaded(true);
       };
 
       script.onload = handleScriptLoad;
+
       document.getElementsByTagName("head")[0].appendChild(script);
 
       return () => {
@@ -50,15 +82,47 @@ export default function OnboardPaypal() {
     getOnboardLinks();
   }, []);
   if (!terapeuta) return;
-  return !links ? (
-    <></>
-  ) : showButton ? (
-    <div dir="ltr" style={{ textAlign: "left" }}>
-      <a data-paypal-button="true" href={`${links[1].href}`} target="_self">
-        Sign up for PayPal
-      </a>
+  // if (!showStatus) return;
+  return (
+    <div>
+      <Title order={4} mb="sm">
+        Método de pago
+      </Title>
+      <PaypalSvg width="15vh" height="auto" />
+      <Stack spacing="md" w="fit-content">
+        {merchantId && (
+          <Group align="center">
+            <Text>merchant_id:</Text>
+            <Text>{merchantId}</Text>
+          </Group>
+        )}
+        {status && (
+          <Group align="center">
+            <Text>Status:</Text>
+            {status === ACTIVO ? (
+              <Badge color="green-nature.9">Activo</Badge>
+            ) : (
+              <Badge>Inactivo</Badge>
+            )}
+          </Group>
+        )}
+        {scriptLoaded &&
+          (!showVincular ? (
+            <Skeleton w="10em" h="2em" />
+          ) : (
+            <div>
+              <Button
+                component="a"
+                variant="siguiente"
+                data-paypal-button="true"
+                href={`${links[1].href}&displayMode=minibrowser`}
+                target="PPFrame"
+              >
+                Vincular paypal
+              </Button>
+            </div>
+          ))}
+      </Stack>
     </div>
-  ) : (
-    <></>
   );
 }
