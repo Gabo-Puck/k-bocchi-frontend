2baa2434ad49ae337496d937c658d3a70a007847
Gabo-Puck <melkorgodo@gmail.com>
2023-07-19 23:01:09 -0600

    Añadido: Añadido toggle audio/video

--- src/pages/Videochat.js
+++ src/pages/Videochat.js
@@ -1,7 +1,22 @@
 import { useEffect, useRef, useState } from "react";
 import Peer from "peerjs";
-import { Box, Container, SimpleGrid, createStyles } from "@mantine/core";
-
+import {
+  ActionIcon,
+  Box,
+  Container,
+  Flex,
+  SimpleGrid,
+  UnstyledButton,
+  createStyles,
+} from "@mantine/core";
+import {
+  FaVolumeMute,
+  FaVolumeUp,
+  FaVideo,
+  FaVideoSlash,
+  FaPhoneSlash,
+} from "react-icons/fa";
+import { useDisclosure } from "@mantine/hooks";
 const useStyles = createStyles((theme) => ({
   videoInsert: {
     width: "100%",
@@ -12,10 +27,14 @@ const useStyles = createStyles((theme) => ({
 export default function Videochat() {
   const [peerId, setPeerId] = useState("");
   const [remotePeerIdValue, setRemotePeerIdValue] = useState("");
+  const [remoteStream, setRemoteStream] = useState();
+  const [userStream, setUserStream] = useState();
   const remoteVideoRef = useRef(null);
   const currentUserVideoRef = useRef(null);
   const peerInstance = useRef(null);
   const { classes, cx } = useStyles();
+  const [audio, { toggle: toggleAudio }] = useDisclosure(true);
+  const [video, { toggle: toggleVideo }] = useDisclosure(true);
   useEffect(() => {
     const peer = new Peer();
 
@@ -31,6 +50,7 @@ export default function Videochat() {
 
       getUserMedia({ video: true, audio: true }, (mediaStream) => {
         currentUserVideoRef.current.srcObject = mediaStream;
+        setUserStream(mediaStream);
         // currentUserVideoRef.current.play();
         call.answer(mediaStream);
         call.on("stream", function (remoteStream) {
@@ -42,7 +62,17 @@ export default function Videochat() {
 
     peerInstance.current = peer;
   }, []);
-
+  useEffect(() => {
+    console.log({ video });
+    if (userStream)
+      userStream.getVideoTracks().forEach((t) => t.enabled = video);
+  }, [video]);
+  useEffect(() => {
+    console.log({ audio });
+    if (userStream) {
+      userStream.getAudioTracks().forEach((t) => t.enabled = audio);
+    }
+  }, [audio]);
   const call = (remotePeerId) => {
     var getUserMedia =
       navigator.getUserMedia ||
@@ -52,7 +82,7 @@ export default function Videochat() {
     getUserMedia({ video: true, audio: true }, (mediaStream) => {
       currentUserVideoRef.current.srcObject = mediaStream;
       // currentUserVideoRef.current.play();
-
+      setUserStream(mediaStream);
       const call = peerInstance.current.call(remotePeerId, mediaStream);
 
       call.on("stream", (remoteStream) => {
@@ -63,7 +93,7 @@ export default function Videochat() {
   };
 
   return (
-    <>
+    <Container>
       <h1>Current user id is {peerId}</h1>
       <input
         type="text"
@@ -73,6 +103,7 @@ export default function Videochat() {
       <button onClick={() => call(remotePeerIdValue)}>Call</button>
       <Container>
         <SimpleGrid
+          w="100%"
           breakpoints={[
             { minWidth: "xs", cols: 1 },
             { minWidth: "sm", cols: 2 },
@@ -94,7 +125,70 @@ export default function Videochat() {
             />
           </Box>
         </SimpleGrid>
+        <Flex gap="md" w="100%" justify="center">
+          <ToggleIcon
+            activeElement={
+              <ControlButtonLlamada
+                actionIconProps={{ size: "3em", variant: "filled" }}
+              >
+                <FaVolumeUp />
+              </ControlButtonLlamada>
+            }
+            desactiveElement={
+              <ControlButtonLlamada
+                actionIconProps={{ size: "3em", variant: "filled" }}
+              >
+                <FaVolumeMute />
+              </ControlButtonLlamada>
+            }
+            value={audio}
+            onClick={() => {
+              toggleAudio();
+            }}
+          />
+          <ToggleIcon
+            activeElement={
+              <ControlButtonLlamada
+                actionIconProps={{ size: "3em", variant: "filled" }}
+              >
+                <FaVideo />
+              </ControlButtonLlamada>
+            }
+            desactiveElement={
+              <ControlButtonLlamada
+                actionIconProps={{ size: "3em", variant: "filled" }}
+              >
+                <FaVideoSlash />
+              </ControlButtonLlamada>
+            }
+            value={video}
+            onClick={() => {
+              toggleVideo();
+            }}
+          />
+          <ControlButtonLlamada
+            actionIconProps={{ size: "3em", variant: "filled", color: "red" }}
+          >
+            <FaPhoneSlash />
+          </ControlButtonLlamada>
+        </Flex>
       </Container>
-    </>
+    </Container>
+  );
+}
+
+function ControlButtonLlamada({ actionIconProps, ...props }) {
+  return (
+    <ActionIcon radius="xl" {...actionIconProps}>
+      {props.children}
+    </ActionIcon>
+  );
+}
+
+function ToggleIcon({ activeElement, desactiveElement, value, onClick }) {
+  return (
+    <UnstyledButton onClick={onClick}>
+      {value ? activeElement : desactiveElement}
+    </UnstyledButton>
   );
 }
